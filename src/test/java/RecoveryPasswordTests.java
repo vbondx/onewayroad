39d4b9e0e49ba1393d1861ae88021dc1835b89e5
vbondx <act35bh>
2017-07-30 23:51:06 +0300

    Auth[DONE], Forgot[IN PROGRESS]

---
+++ src/test/java/RecoveryPasswordTests.java
@@ -0,0 +1,69 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class RecoveryPasswordTests extends BaseTest {
+
+    @Test
+
+    public void incorrectEmail() {
+        BasePage basePage = new BasePage(driver);
+        basePage.goToUnAuthProfilePage();
+        UnAuthProfilePage unAuthProfilePage = new UnAuthProfilePage(driver);
+        unAuthProfilePage.goToSignInPage();
+        SignInPage signInPage = new SignInPage(driver);
+        signInPage.goToForgotPasswordPage();
+        RecoveryPasswordPage forgotPasswordPage = new RecoveryPasswordPage(driver);
+        forgotPasswordPage.clearData();
+        forgotPasswordPage.incorrectEmailOrPhone("minnnune@gmail,com");
+        Assert.assertEquals(forgotPasswordPage.inputErrorEmailDisplayed(), true);
+    }
+
+    @Test
+
+    public void phoneNumberSmallerThanTwelve() {
+        BasePage basePage = new BasePage(driver);
+        basePage.goToUnAuthProfilePage();
+        UnAuthProfilePage unAuthProfilePage = new UnAuthProfilePage(driver);
+        unAuthProfilePage.goToSignInPage();
+        SignInPage signInPage = new SignInPage(driver);
+        signInPage.goToForgotPasswordPage();
+        RecoveryPasswordPage forgotPasswordPage = new RecoveryPasswordPage(driver);
+        forgotPasswordPage.clearData();
+        forgotPasswordPage.incorrectEmailOrPhone("+38063235034");
+        Assert.assertEquals(forgotPasswordPage.inputErrorPhoneDisplayed(),true);
+    }
+
+    @Test
+
+    public void phoneNumberLongerThanTwelve() {
+        BasePage basePage = new BasePage(driver);
+        basePage.goToUnAuthProfilePage();
+        UnAuthProfilePage unAuthProfilePage = new UnAuthProfilePage(driver);
+        unAuthProfilePage.goToSignInPage();
+        SignInPage signInPage = new SignInPage(driver);
+        signInPage.goToForgotPasswordPage();
+        RecoveryPasswordPage forgotPasswordPage = new RecoveryPasswordPage(driver);
+        forgotPasswordPage.clearData();
+        forgotPasswordPage.incorrectEmailOrPhone("+3806323503422");
+        forgotPasswordPage.submitNotValidData();
+        Assert.assertEquals(forgotPasswordPage.emailOrPhoneFieldDisplayed(),true);
+    }
+
+    @Test
+
+    public void validDataRecovery() {
+        BasePage basePage = new BasePage(driver);
+        basePage.goToUnAuthProfilePage();
+        UnAuthProfilePage unAuthProfilePage = new UnAuthProfilePage(driver);
+        unAuthProfilePage.goToSignInPage();
+        SignInPage signInPage = new SignInPage(driver);
+        signInPage.goToForgotPasswordPage();
+        RecoveryPasswordPage forgotPasswordPage = new RecoveryPasswordPage(driver);
+        forgotPasswordPage.clearData();
+        forgotPasswordPage.incorrectEmailOrPhone("+380632350342");
+        forgotPasswordPage.goToNextPage();
+        RecoveryPasswordNextPage recoveryPasswordNextPage = new RecoveryPasswordNextPage(driver);
+        Assert.assertEquals(recoveryPasswordNextPage.instructionTextDisplayed(),true);
+    }
+
+}
