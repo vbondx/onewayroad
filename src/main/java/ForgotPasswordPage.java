5ad5fe95ad20453da0045d17cdd3230fc8261ec9
vbondx <vbondx@gmail.com>
2017-08-03 15:10:07 +0300

    - fixed tests

---
+++ src/main/java/ForgotPasswordPage.java
@@ -0,0 +1,11 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+
+/**
+ * Created by v.bondarenko on 03.08.17.
+ */
+public class ForgotPasswordPage extends BasePage {
+    public ForgotPasswordPage(AppiumDriver<MobileElement> driver) {
+        super(driver);
+    }
+}
